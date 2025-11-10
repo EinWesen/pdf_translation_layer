4384073f5d4830928be454c3b8997f6c50e63769
EinWesen <einwesen@gmail.com>
2025-11-10 20:51:18 +0100

    Try to keep font layout

--- translate_cmd.py
+++ translate_cmd.py
@@ -3,9 +3,12 @@
 from abc import abstractmethod
 import os
 import argparse
+import re
+from collections import Counter
 
 import pymupdf
 from tqdm import tqdm
+import tempfile
 
 class TextTranslator:
     def __init__(self, lang_from:str, lang_to:str):
@@ -39,7 +42,7 @@ class OpenAiCompatibleTranslator(TextTranslator):
 
         client = openai.OpenAI(api_key=self.api_key, base_url=self.base_url)
 
-        prompt = f'Translate the text below into {self.lang_to} and return the translated text only.\nText: "{text}"'
+        prompt = f'Translate the text below into {self.lang_to} while keeping line breaks and eturn the translated text only.\nText: {text}"'
 
         response = client.chat.completions.create(model=self.model, messages=[{ "role": "user", "content": prompt }])
         
@@ -51,8 +54,109 @@ TRANSLATORS = {
     'openai': OpenAiCompatibleTranslator,
 }
 
-def translate_pdf(input_file: str, source_lang: str, target_lang: str, layer: str = "Text", 
-                 translator_name: str = "google", text_color: str = "darkred", keep_original: bool = True):
+def extract_embedded_fonts(doc):
+    """
+    Extract all embedded fonts from a PyMuPDF document and save to temporary files.
+
+    Args:
+        doc (fitz.Document): Opened PyMuPDF document.
+
+    Returns:
+        dict: {font_name: temp_file_path}
+    """
+    fonts_dict = {}
+    fonts_found = []
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        fonts = page.get_fonts(full=True)
+
+        for f in fonts:
+            xref = f[0]
+            font_name = f[3]  # basefont name
+            embedded = f[4]   # True if embedded
+
+            if not embedded or font_name in fonts_found:
+                # Skip non-embedded fonts or already extracted fonts
+                continue
+
+            fonts_found.append(font_name)
+            font_data = doc.extract_font(xref, named=True)
+
+            if not font_data:
+                print(f"Warning: Could not extract embedded font: {font_name} and will be ignored")
+                fonts_dict[font_name] = None
+                continue
+
+            glyphs = font_data.get("glyphs")
+
+            if not glyphs or len(glyphs) == 0:
+                print(f"Warning: Embedded font '{font_name}' is likely glyph-less or vector-only and will be ignored")
+                fonts_dict[font_name] = None
+                continue
+
+            ext = font_data.get("ext", "ttf")
+            with tempfile.NamedTemporaryFile(suffix=f".{ext}", delete=False) as tmp:
+                tmp.write(font_data["content"])
+                tmp_path = tmp.name
+
+            fonts_dict[font_name] = tmp_path
+            print(f"INFO: Embedded font '{font_name}' found")
+        
+    return fonts_dict
+
+def extract_blocks(page, text_flags):
+    #blocks = page.get_text("blocks", flags=textflags)
+    #text_dicts = page.get_text("dict", flags=textflags)
+    
+    text_dict = page.get_text("dict", flags=text_flags)
+    blocks = []
+
+    for block in text_dict["blocks"]:
+        if "lines" not in block:
+            continue  # skip non-text blocks
+
+        bbox = block["bbox"]
+        block_text = ""
+        font_sizes = []
+        fonts = []
+
+        for line in block["lines"]:
+            for span in line["spans"]:                
+                block_text += span["text"]
+                font_sizes.append(span["size"])
+                fonts.append(span["font"])
+            #block_text += '\r\n'
+
+        if not block_text.strip():
+            continue  # skip empty text blocks
+
+        # average font size for this block
+        avg_font_size = sum(font_sizes) / len(font_sizes) if font_sizes else 0        
+        #avg_font_size = 11
+        # most common font in this block
+        most_common_font = Counter(fonts).most_common(1)[0][0] if fonts else None
+
+        blocks.append({
+            "bbox": bbox,
+            "text": block_text,
+            "avg_font_size": avg_font_size,
+            "common_font": most_common_font            
+        })    
+    return blocks
+
+def is_valid_text(text:str)->bool:
+    return not text.replace('\r', '').replace('\n', '').replace(' ', '').isdigit()
+
+def prepare_pdf_text(text:str)->str:
+    return re.sub(r'(?<!\\r)\\n', r'\\r\\n', text)
+
+def insert_text_block(page, fontsize=11, **kwargs):
+    while page.insert_textbox(fontsize=fontsize, **kwargs) < 0 and fontsize > 0:
+        fontsize -= 1
+        if fontsize <= 0: print("Could not render text")
+
+
+def translate_pdf(input_file: str, source_lang: str, target_lang: str, layer: str = "Text", translator_name: str = "google", text_color: str = "darkred", keep_original: bool = True):
     """
     Translate a PDF file from source language to target language
     
@@ -97,6 +201,7 @@ def translate_pdf(input_file: str, source_lang: str, target_lang: str, layer: st
 
     # Open the document
     doc = pymupdf.open(input_file)
+    embedded_fonts = extract_embedded_fonts(doc)
 
     # Define an Optional Content layer for translation
     ocg_trans = doc.add_ocg(layer, on=True)
@@ -106,41 +211,56 @@ def translate_pdf(input_file: str, source_lang: str, target_lang: str, layer: st
         ocg_orig = doc.add_ocg("Original", on=False)
 
     # Iterate over all pages
-    for page in tqdm(doc, desc='Translating page...'):
+    for page_index, page in enumerate(tqdm(doc, desc='Translating page...')):        
         # Extract text grouped like lines in a paragraph.
-        blocks = page.get_text("blocks", flags=textflags)
+        blocks = extract_blocks(page,textflags)
 
         # Every block of text is contained in a rectangle ("bbox")
-        for block in tqdm(blocks, desc='Translating block...', leave=False):
-            bbox = block[:4]  # area containing the text
-            text = block[4]  # the text of this block
+        for block in tqdm(blocks, desc='Translating bocks...', leave=False):
+            bbox = block['bbox']  # area containing the text
+            text = block['text']  # the text of this block
 
-            # Invoke the actual translation
-            translated = translator.translate_text(text)
+            if is_valid_text(text):
 
-            if not keep_original:
-                # Move original text to hidden layer
-                page.insert_htmlbox(
-                    bbox,
-                    text,
-                    css="* {font-family: sans-serif;}",
-                    oc=ocg_orig
+                # Invoke the actual translation
+                translated = prepare_pdf_text(translator.translate_text(text))
+
+                if not keep_original:
+                    # Move original text to hidden layer
+                    page.insert_htmlbox(
+                        bbox,
+                        text,
+                        css="* {font-family: sans-serif;}",
+                        oc=ocg_orig
+                    )
+                    # Clear original text area in base layer
+                    page.draw_rect(bbox, color=None, fill=WHITE)
+                else:
+                    # Cover the original text only in translation layer
+                    page.draw_rect(bbox, color=None, fill=WHITE, oc=ocg_trans)
+
+                # Write the translated text in specified color
+                font_name = block['common_font']
+                font_file_path=embedded_fonts.get(font_name,None)
+                
+                if font_name not in embedded_fonts or font_file_path is None:
+                    #if font_name not in embedded_fonts:
+                    #    print(f"WARNING: {font_name} not found")
+                    font_name = 'helv'
+                            
+                insert_text_block(
+                    page,
+                    rect=bbox,
+                    buffer=translated,
+                    fontsize=block['avg_font_size'], 
+                    fontname=font_name,
+                    fontfile=font_file_path,
+                    oc=ocg_trans,
+                    color=rgb_color
                 )
-                # Clear original text area in base layer
-                page.draw_rect(bbox, color=None, fill=WHITE)
-            else:
-                # Cover the original text only in translation layer
-                page.draw_rect(bbox, color=None, fill=WHITE, oc=ocg_trans)
 
-            # Write the translated text in specified color
-            page.insert_htmlbox(
-                bbox,
-                translated,
-                css=f"* {{font-family: sans-serif; color: rgb({int(rgb_color[0]*255)}, {int(rgb_color[1]*255)}, {int(rgb_color[2]*255)});}}",
-                oc=ocg_trans
-            )
 
-    doc.subset_fonts()
+    #doc.subset_fonts()
     doc.ez_save(output_file)
     print(f"Translated PDF saved as: {output_file}")
 
